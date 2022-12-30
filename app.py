bd3498741910372adc5a6dac5788e8b12a880c8b
AndreTamutis <andre.tamutis@amedigitl.com>
2022-12-29 21:34:15 -0300

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Atividade Devops Andre Tamutis"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
-   
-    port = os.getenv('PORT')
-    app.run('0.0.0.0', port=port)
