2f0899bb4bb36adad8f3447eafcec1e3a5dcec22
André Luiz <69218528+AndreTamutis@users.noreply.github.com>
2023-03-11 13:33:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return  "Atividade Devops Andre Tamutis"
+    return  "Atividade Devops André Tamutis"
 
 if __name__ == '__main__':
     app.run()
